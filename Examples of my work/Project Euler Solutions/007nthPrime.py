21abfeed1132e40cd80f542938da9bbb41668b3e
Xavier O'Rourke <xavier.orourke@gmail.com>
2017-04-26 13:25:17 +1000

    Adding comments to some ProjectEuler solutions

--- Examples of my work/Project Euler Solutions/007nthPrime.py
+++ Examples of my work/Project Euler Solutions/007nthPrime.py
@@ -1,5 +1,6 @@
 """
 This question asks: what is the 10 001st prime number?
+My implementation takes a few seconds to run...
 """
 
 n = 10001
@@ -7,20 +8,19 @@ n = 10001
 primes = set()
 primes.add(2)
 primes.add(3)
-largestPrime = 2
+largestPrime = 3
 while len(primes) < n:
-    i = largestPrime
-    repeat = True
-    while repeat:
-        i += 2
-        prime = True
+    i = largestPrime + 2
+    while not i in primes: # Check if i is prime, if it isn't add two and check again
+        isPrime = True
         for p in primes:
             if i%p == 0:
-                prime = False
+                isPrime = False
                 break
-        if prime:
+        if isPrime:
             primes.add(i)
             largestPrime = i
-            repeat = False
+        else:
+            i += 2
 
 print(largestPrime)
--- Examples of my work/Project Euler Solutions/010SummationOfPrimes.py
+++ Examples of my work/Project Euler Solutions/010SummationOfPrimes.py
@@ -1,15 +1,29 @@
+"""
+This question asks for the sum of all primes below two million. My implementation takes around
+fifteen seconds on my laptop so please be patient :( An alternative implementation would be to
+sum the primes up as we generate them and not keep the list in memory. This is slightly slower
+but would save on space.
+"""
+
 n = 2000000
 
-sum = 5
-i = 3
-while i < n-1:
-        i += 2
+import time
+start_time = time.time()
+def build_list_of_primes(n):
+    """Returns a list containing all the primes less than or equal to n"""
+    primes = []
+    for i in range(2,n+1):
         prime = True
-        for j in range(2, int(i**0.5)+1):
-            if i%j == 0:
+        for p in primes:
+            if i%p == 0:
                 prime = False
                 break
+            if p >= i**0.5: # No need to look for prime factors beyond the square root
+                break
         if prime:
-            sum += i
+            primes.append(i)
 
-print(sum)
+    return primes
+
+print(sum(build_list_of_primes(n)))
+print("--- %s seconds ---" % (time.time() - start_time))
